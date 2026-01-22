3dcfa1202e4d157a849cb6729e9a1cc036652dcb
eprian <eprian89@gmail.com>
2026-01-22 17:00:52 +0100

    flask actualizado

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "¡Hola Mundo desde Flask!"
+    return "¡Bienvenidos a mi mundo!"
 
 if __name__ == "__main__":
     app.run(debug=True)
