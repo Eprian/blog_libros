975fb0d040a10b066f6745af4cb20f6ef127a350
eprian <eprian89@gmail.com>
2026-01-22 21:33:50 +0100

    rederizar las páginas

--- app.py
+++ app.py
@@ -4,21 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", content='')# Contenido inicial
 
 @app.route("/contacto")
 def contact():
-    return render_template("contact.html")
+    return render_template("index.html", content="<h1>Contacto</h1><p>Aquí puedes contactarnos...</p>")
 
 @app.route("/acerca_de")
 def about():
-    return render_template("about.html")
+    return render_template("index.html", content="<h1>Acerca de</h1><p>Información sobre el blog...</p>")
 
 @app.route("/<page_name>")
 def dynamic_page(page_name):
     # Intenta renderizar la página solicitada
     try:
-        return render_template(f"{page_name}.html", content='')
+        return render_template("index.html", content=f"<h1>{page_name}</h1><p>Contenido de la página...</p>")
     except Exception as e:
         return "Página no encontrada", 404
 
