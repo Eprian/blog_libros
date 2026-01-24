5bccbc6e17685c7d31a6653f1f30f991404ddd2d
eprian <eprian89@gmail.com>
2026-01-24 20:08:27 +0100

    app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-content = ""  # Define la variable content a nivel global
+
 def inicio():
     return render_template(
         "index.html",
