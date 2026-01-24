3a4573fefee122fcc940dd099498df3ece95d7ee
eprian <eprian89@gmail.com>
2026-01-24 20:15:04 +0100

    cambios app y about

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
 def inicio():
     return render_template(
         "index.html",
@@ -28,14 +29,6 @@ def contacto():
         "index.html",
         content="<h1>Contacto</h1>"
     )
-
-@app.route("/<page_name>")
-def dynamic_page(page_name):
-    # Intenta renderizar la página solicitada
-    try:
-        return render_template("index.html", content=f"<h1>{page_name}</h1><p>Contenido de la página...</p>")
-    except Exception as e:
-        return "Página no encontrada", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
